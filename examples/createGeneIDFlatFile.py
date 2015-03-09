e1ad52bd56de62daa0f2bfd86681b59cf747a38f
Kendal Harland <kharland2011@gmail.com>
2015-03-09 11:36:32 -0500

    added param handling on command line

--- examples/createGeneIDFlatFile.py
+++ examples/createGeneIDFlatFile.py
@@ -1,10 +1,15 @@
-import pypug
 import sys
+import os
+import ConfigParser
+from datetime import date
+import zipfile
+import gzip
 import json
 import math
 import mysql.connector
 from mysql.connector import errorcode
 from mysql.connector.constants import ClientFlag
+import pypug
 
 """
 File 1
@@ -15,22 +20,24 @@ aid
 aid
 ...
 
-for id in geneids
-  aids <- getAIDSFor(id)
-  output ">{id}"
-  for aid in aids
-    output "aid\n"
+pypug version
+database version
 """
 
-user, passwd, db = "kharland", "@mcdaniel1", "ccb"
-geneids = [1815, 1816]
-dataFile1 = "file1.txt"
-dataFile2 = "file2.txt"
+host, user, passwd, db = sys.argv[1:5]
+geneids = sys.argv[5:]#[1815, 1816]
 
-print "starting file 1"
+pypugFile1 = "geneid.aid.pypug.txt"
+pypugFile2 = "geneid.fullassay.pypug.txt"
+ccbFile1   = "geneid.aid.ccb.txt"
+ccbFile2   = "geneid.fullassay.ccb.txt"
 
-with open(dataFile1, 'w') as outfile:
+# -- PyPug File 1 --
+
+with open(pypugFile1, 'w') as outfile:
   for i in range(0, len(geneids)):
+    # Get all aids associated with this geneid from PUG
+    # and write the geneid header followed by each id
     aids = pypug.getAIDsFromGeneID(geneids[i])
     outfile.write(">%s\n" % geneids[i])
     for j in range(0, len(aids)):
@@ -39,6 +46,29 @@ with open(dataFile1, 'w') as outfile:
       sys.stdout.flush()
       outfile.write("%s\n" % aids[j])
   sys.stdout.write('\n')
+
+# -- CCB File 1 -- 
+
+with open(ccbFile1, 'w') as outfile:
+  # Get all assay ids associated with this geneid from the database
+  cnx = mysql.connector.connect(host=host, user=user, passwd=passwd, db=db, client_flags=[ClientFlag.LOCAL_FILES])
+  cursor = cnx.cursor()
+  for i in range(0, len(geneids)):
+    outfile.write(">%s\n" % geneids[i])
+    cursor.execute((
+      "SELECT DISTINCT(assay_id) "
+      "FROM Aid2GiGeneidAccessionUniprot "
+      "WHERE gene_id='%s';"), geneids[i])
+    # Load the list of aids
+    aids = map(itemgetter(0), cursor.fetchall())
+    for j in range(0, len(aids)):
+      sys.stdout.write(("\rWriting aid (%03d/%03d) for gene (%02d/%02d)" %
+        (j+1, len(aids), i+1, len(geneids))))
+      sys.stdout.flush()
+      outfile.write("%s\n" % aids[j])
+  sys.stdout.write('\n')
+  cnx.close()
+
 
 """
 File 2
@@ -59,10 +89,58 @@ while there are still geneids
     output "{CID}\t{SMILES}\t{outcome}\n"
 """
 
-print "starting file 2"
+# -- PyPug File 2 --
 
-# with open(dataFile1, 'r') as infile:
-#   with open(dataFile2, 'w') as outfile:
+with open(pypugFile1, 'r') as infile:
+  with open(pypugFile2, 'w') as outfile:
+    i = 0
+    for line in infile:
+      i += 1
+      sys.stdout.write("\r> processing lines (%03d)" % i)
+      sys.stdout.flush()
+      line = line.rstrip()
+      if line[0] is '>':
+        outfile.write("%s\n" % line) # geneid
+      else:
+        aid = line
+        assay = pypug.getAssayFromSIDs(aid)
+        outfile.write("$%s\n" % aid)
+        for sid, cid, outcome in zip(assay["PUBCHEM_SID"], assay["PUBCHEM_CID"], assay["PUBCHEM_ACTIVITY_OUTCOME"]):
+          if math.isnan(cid): # cid is unavailable
+            cid, smiles = "","" # empty values since no cid
+          else:
+            smiles = pypug.getCanonicalSMILESFromCID(cid)
+          outfile.write("%s\t%s\t%s\t" % sid, cid, smiles)
+          outfile.write("\n");
+    sys.stdout.write('\n')
+
+# -- CCB File 2 --
+
+with open(ccbFile1, 'r') as infile:
+  cnx = mysql.connector.connect(host=host, user=user, passwd=passwd, db=db, client_flags=[ClientFlag.LOCAL_FILES])
+  cursor = cnx.cursor()
+  with open(ccbFile2, 'w') as outfile:
+    i = 0
+    for line in infile:
+      i += 1
+    sys.stdout.write("\r> processing lines (%03d)" % i)
+    sys.stdout.flush()
+    line = line.rstrip()
+    if line[0] is '>':
+      outfile.write("%s\n" % line) # geneid
+    else:
+      aid = line
+      cursor.execute((
+        "SELECT Bioassays.substance_id, Bioassays.activity_outcome,"
+        "       Substance_id_compound_id.compound_id,"
+        "FROM Bioassays "
+        "WHERE Bioassays.aid='%s' "
+        "AND Substance_id_compound_id.substance_id=Bioassays.substance_id"), aid)
+      print cursor.fetchall()
+
+
+# with open(pypugFile1, 'r') as infile:
+#   with open(pypugFile2, 'w') as outfile:
 #     i = 0
 #     for line in infile:
 #       i += 1
@@ -74,9 +152,9 @@ print "starting file 2"
 #       else:
 #         aid = line
 #         assay = pypug.getAssayFromSIDs(aid)
-#         description = json.dumps(pypug.getAssayDescriptionFromAID(aid))
+#         #description = json.dumps(pypug.getAssayDescriptionFromAID(aid))
 #         outfile.write("$%s\n" % aid)
-#         outfile.write("%s\n" % description)
+#         #outfile.write("%s\n" % description)
 #         for sid, cid, outcome in zip(assay["PUBCHEM_SID"], assay["PUBCHEM_CID"], assay["PUBCHEM_ACTIVITY_OUTCOME"]):
 #           if math.isnan(cid): # cid is unavailable
 #             cid, smiles = "","" # empty values since no cid
@@ -87,31 +165,3 @@ print "starting file 2"
 #           outfile.write("%s\t" % smiles)
 #           outfile.write("\n");
 #     sys.stdout.write('\n')
-
-print "loading mysql table with descriptions"
-
-with open(dataFile1, 'r') as infile:
-  with open(dataFile2, 'w') as outfile:
-    i = 0
-    for line in infile:
-      i += 1
-      sys.stdout.write("\r> processing lines (%03d)" % i)
-      sys.stdout.flush()
-      line = line.rstrip()
-      if line[0] is '>':
-        pass
-      else:
-        aid = line
-        # Remove whitespace in response using json.loads and json.dumps
-        description = json.dumps(json.loads(
-          pypug.getAssayDescriptionFromAID(aid).encode('utf-8')), 
-          separators=(',', ': '))
-        try:
-          cnx = mysql.connector.connect(
-            user=user, passwd=passwd, db=db, client_flags=[ClientFlag.LOCAL_FILES])
-          cursor = cnx.cursor()
-          query = "INSERT INTO `Assay_id_assay_description`(assay_id, assay_description) VALUES(%s,%s);"
-          cursor.execute(query, (aid, description))
-          cnx.commit()
-        except mysql.connector.Error as e:
-          sys.stderr.write("x failed loading data: %s\n" % e)
